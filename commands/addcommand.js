76c23a48698c80272d31f997c4879476280442ef
KyllianGamer <kylleke@telenet.be>
2020-06-16 21:59:48 +0200

    addcomand gen1
    The first attempt and test at !addcommand.

---
+++ commands/addcommand.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const cmds = require("./commands.json");
+
+module.exports.run = async (client, message, args) => {
+
+    if (!args[0]) return;
+    if (!args[1]) return;
+
+    if (!cmds[args[0]]) return message.reply("this command already exists!");
+    cmds[args[0]] = message.content.slice(10 + args[0].length);
+
+    fs.writeFile('./commands.json', JSON.stringify(cmds));
+
+}
+
+module.exports.help = {
+    name: "addcommand"
+}
