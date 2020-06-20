c4c9493b309c1040a8148cce3548a2c7cb657d70
KyllianGamer <kylleke@telenet.be>
2020-06-20 20:37:41 +0200

    Update addcommand.js

--- commands/addcommand.js
+++ commands/addcommand.js
@@ -7,10 +7,10 @@ module.exports.run = async (client, message, args) => {
     if (!args[0]) return;
     if (!args[1]) return;
 
-    if (!cmds[args[0]]) return message.reply("this command already exists!");
+    if (cmds[args[0]]) return message.reply("this command already exists!");
     cmds[args[0]] = message.content.slice(10 + args[0].length);
 
-    fs.writeFile('./commands.json', JSON.stringify(cmds));
+    fs.writeFileSync('./commands.json', JSON.stringify(cmds));
 
 }
 
