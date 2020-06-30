db714c275530c581b025676408d286180e6b919b
KyllianGamer <kylleke@telenet.be>
2020-06-30 14:44:18 +0200

    Update randompost.js

--- commands/randompost.js
+++ commands/randompost.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
 
     const memeEmbed = new Discord.MessageEmbed()
         .setColor("RANDOM")
-        .setImage(meme)
+        .setImage(post)
         .setTitle(`POST`)
         .setFooter(`/r/${args[0]}`);
 
