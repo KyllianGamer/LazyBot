c700816a6de4fc21dbf9ab2a5daf9e61255adf69
KyllianGamer <kylleke@telenet.be>
2020-06-30 14:43:05 +0200

    Create randompost.js

---
+++ commands/randompost.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const randomPup = require("random-puppy");
+
+module.exports.run = async (client, message, args) => {
+
+    if (message.channel.id != "727488241954979886") return;
+
+    if(!args[0]) return;
+    const post = await randomPup(args[0]);
+    if (!post) return;
+
+    const memeEmbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setImage(meme)
+        .setTitle(`POST`)
+        .setFooter(`/r/${args[0]}`);
+
+    message.channel.send(memeEmbed);
+}
+
+module.exports.help = {
+    name: "randompost"
+}
